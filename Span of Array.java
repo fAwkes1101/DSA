3d2f7fe9c411b68d4d1f9fe34b3125e0fc851ade
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2021-11-12 17:42:33 +0530

    Update Span of Array.java

--- Span of Array.java
+++ Span of Array.java
@@ -3,14 +3,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int[] arr= new int[8];
+        int n = scn.nextInt();
+        int[] arr= new int[n];
 
-        for(int i=0;i<8; i++){
+        for(int i=0;i<n; i++){
             arr[i] = scn.nextInt();
         }
         int min= Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for(int i=0;i<8;i++){
+        for(int i=0;i<n;i++){
 
             if(arr[i]<min)
                 min=arr[i];
