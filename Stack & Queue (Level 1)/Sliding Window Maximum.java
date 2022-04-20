de1d0321f2b8884f334c1de2dec2751cc861e988
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2022-04-20 22:32:56 +0530

    Update Sliding Window Maximum.java

--- Stack & Queue (Level 1)/Sliding Window Maximum.java
+++ Stack & Queue (Level 1)/Sliding Window Maximum.java
@@ -1,3 +1,4 @@
+//1. Using Deque
 import java.io.*;
 import java.util.*;
 
@@ -34,3 +35,59 @@ public class Main {
     }
   }
 }
+
+//2.Using Stack-> next greater element concept
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    // code
+
+    // nge begin
+   int[] nge= new int[arr.length];
+   Stack<Integer> st = new Stack<>();
+   st.push(arr.length-1); 
+   nge[arr.length-1] = arr.length;
+   
+   for(int i=arr.length-2; i>=0; i--){
+       while(st.size()>0 && arr[i]>=arr[st.peek()]){
+           st.pop();
+       }
+       if(st.size()==0){
+           nge[i]=arr.length; // sabko pop kra diya apne se phle wale elements ko to hamara next greater exit nhi karta
+       }
+       else{
+           nge[i]= st.peek();
+       }       
+       st.push(i);
+   }
+   
+   int j=0; //nge in window pe jump lagaega
+   for(int i=0; i<=arr.length-k;i++){
+       if(j<i){
+           j=i;
+       }
+       
+       while(nge[j]<i+k){
+           j = nge[j];
+       }
+       System.out.println(arr[j]);
+   }
+   
+ }
+}
+                        
+                                
+                                
