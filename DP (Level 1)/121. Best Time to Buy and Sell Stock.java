e12c9a5c531ff16e4cdb125fcb9381e4b6b9be84
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2022-05-12 23:27:03 +0530

    Update 121. Best Time to Buy and Sell Stock.java

--- DP (Level 1)/121. Best Time to Buy and Sell Stock.java
+++ DP (Level 1)/121. Best Time to Buy and Sell Stock.java
@@ -1,4 +1,4 @@
-class Solution {
+//Considering each day as potential selling day, TC: O(n) SC: O(1)
     public int maxProfit(int[] prices) {
         int profit = 0;
         int max = -1;
@@ -10,4 +10,19 @@ class Solution {
         }
         return profit;
     }
-}
+
+
+//Considering each day as potential buying day, TC: O(n) SC: O(1)
+ public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buying = prices[0];
+        
+        for(int i=0 ; i<prices.length; i++){
+            buying = Math.min(buying, prices[i]);
+            int selling = prices[i];
+            
+            profit= Math.max(profit, selling-buying);
+            
+        }
+        return profit;
+    }
