88f1d9795aec05c243c0b6c5223631d761474d07
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2022-05-26 10:49:47 +0530

    Update 674. Longest Continuous Increasing Subsequence.java

--- DP (Level 1)/674. Longest Continuous Increasing Subsequence.java
+++ DP (Level 1)/674. Longest Continuous Increasing Subsequence.java
@@ -1,4 +1,4 @@
-class Solution {
+//Memoisation Time and space: O(n*m)
     public int memo(int[][] grid,int i, int j, int[][] dp){
         if(i==grid.length-1 && j==grid[0].length-1){
             return grid[i][j];
@@ -24,3 +24,27 @@ class Solution {
         return memo(grid, 0, 0, dp);
     }
 }
+
+//Tabulation: Time and Space O(n*m)
+   public int minPathSum(int[][] grid) {
+        int n = grid.length; int m= grid[0].length;
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                dp[i][j]= Integer.MAX_VALUE;
+            }
+        }
+        
+        for(int i= n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(i==n-1 && j==m-1){
+                dp[i][j] = grid[i][j];
+                continue;
+                }
+                dp[i][j]= Math.min(dp[i+1][j], dp[i][j+1]) + grid[i][j];
+            }
+        }
+       return dp[0][0];
+    }
+
+// Spaceoptimisation possible using 1d dp
