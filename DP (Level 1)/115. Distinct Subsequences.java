ee527e37e4f2937aaac12f78506867c7777487d5
Abhishek Yadav <86714900+fAwkes1101@users.noreply.github.com>
2022-06-02 18:07:19 +0530

    Create 115. Distinct Subsequences.java

---
+++ DP (Level 1)/115. Distinct Subsequences.java
@@ -0,0 +1,27 @@
+class Solution{
+    
+    public int memo(int i, int j, String s, String t, int[][]dp){
+        if(j == t.length()) return 1;
+        if(i ==s.length()) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        char ofS = s.charAt(i);
+        char ofT = t.charAt(j);
+        
+        if(ofS==ofT){
+            return dp[i][j] = (memo(i+1, j+1, s, t, dp) + memo(i+1, j, s, t,dp));
+        }
+        return dp[i][j] = memo(i+1, j, s, t,dp);
+    }
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length()+1][t.length()+1];
+        
+        for(int i=0; i<=s.length(); i++){
+            for(int j=0; j<=t.length(); j++){
+                dp[i][j]=-1;
+            }
+        }
+        return memo(0, 0, s, t, dp);
+    }
+}
